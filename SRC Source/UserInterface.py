c4b2c0196a1441de67fcf28245432e92ee4648e5
Ivan Lamb <31833465+ToastedTaco@users.noreply.github.com>
2022-11-27 16:53:53 -0500

    Update UserInterface.py
    Still need to add dataframe integration, and sensordata integration

--- SRC Source/UserInterface.py
+++ SRC Source/UserInterface.py
@@ -9,34 +9,122 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class UserInterface(tk.Tk):
+
     def __init__(self):
         super().__init__()
         self.title('Sensor Data')
         self.geometry('1000x800')
+        frame = Frame(self)
+
+        frame.pack(side=tk.TOP, expand=1, anchor='w')
+
+        figure = Figure(figsize=(6, 4), dpi=100)
+
+        tempFigure_canvas = FigureCanvasTkAgg(figure, self)
+
+        tempFigure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        temp_summarize = Button(frame, text='Summarize', command= self.temp_summarize())
+        aggregate = Button(frame, text='Aggregate')
+        temp_summarize.pack(side = RIGHT)
+        aggregate.pack(side = RIGHT)
+        self.axes = figure.add_subplot()
+
+        frame = Frame(self)
+
+        frame.pack(side=tk.TOP, expand=1, anchor='w')
 
 
-        for x in range(3):
-            frame = Frame(self)
+        acc_magFrame_canvas = FigureCanvasTkAgg(figure, self)
 
-            frame.pack(side=tk.TOP, expand=1, anchor='w')
+        acc_magFrame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        acc_summarize = Button(frame, text='Summarize', command=self.acc_mag_summarize())
+        acc_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
 
-            figure = Figure(figsize=(6, 4), dpi=100)
+        on_wrist_Frame = Frame(self)
 
-            figure_canvas = FigureCanvasTkAgg(figure, self)
+        on_wrist_Frame.pack(side=tk.TOP, expand=1, anchor='w')
 
-            figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        on_wrist_Frame_canvas = FigureCanvasTkAgg(figure, self)
 
-            button1 = Button(frame, text='Summarize')
-            button1.pack(side = RIGHT)
-            button2 = Button(frame, text='Aggregate')
-            button2.pack(side = RIGHT)
-            self.axes = figure.add_subplot()
+        on_wrist_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        on_wrist_summarize = Button(on_wrist_Frame, text='Summarize', command=self.on_wrist_summarize())
+        on_wrist_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
+
+        step_count_Frame = Frame(self)
+
+        step_count_Frame.pack(side=tk.TOP, expand=1, anchor='w')
+
+        step_count_Frame_canvas = FigureCanvasTkAgg(figure, self)
+
+        step_count_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        step_count_summarize = Button(step_count_Frame, text='Summarize', command=self.step_count_summarize())
+        step_count_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
+
+        rest_Frame = Frame(self)
+
+        rest_Frame.pack(side=tk.TOP, expand=1, anchor='w')
+
+        rest_Frame_canvas = FigureCanvasTkAgg(figure, self)
+
+        rest_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        rest_summarize = Button(rest_Frame, text='Summarize', command=self.rest_summarize())
+        rest_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
+
+        EDA_Frame = Frame(self)
+
+        EDA_Frame.pack(side=tk.TOP, expand=1, anchor='w')
 
 
-            # add data here
+        EDA_Frame_canvas = FigureCanvasTkAgg(figure, self)
 
-            # function to add data to graphs
-            # self.df= pd.DataFrame(data, columns=['language', 'popularity'])
+        EDA_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        EDA_summarize = Button(EDA_Frame, text='Summarize', command=self.EDA_summarize())
+        EDA_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
+
+        Movement_Frame = Frame(self)
+
+        Movement_Frame.pack(side=tk.TOP, expand=1, anchor='w')
+
+        Movement_Frame_canvas = FigureCanvasTkAgg(figure, self)
+
+        Movement_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        Movement_summarize = Button(Movement_Frame, text='Summarize', command=self.Movement_summarize())
+        Movement_summarize.pack(side=RIGHT)
+        self.axes = figure.add_subplot()
+
+        E
+        # add data here
+
+        # function to add data to graphs
+        # self.df= pd.DataFrame(data, columns=[' ', ' '])
+
+        # temp
+        # acc_mag
+        # on_wrist
+        # step_count
+        # rest
+        # EDA
+        # Movement intensity
+
+    def temp_summarize(self):
+        pass
+    def acc_mag_summarize(self):
+        pass
+    def on_wrist_summarize(self):
+        pass
+    def step_count_summarize(self):
+        pass
+    def rest_summarize(self):
+        pass
+    def EDA_summarize(self):
+        pass
+    def Movement_summarize(self):
+        pass
 
     def callback(self):
         self.axes.clear()
